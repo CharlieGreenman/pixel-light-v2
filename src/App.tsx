37b03cf1e65241f67c62b20b1462ce9dc970c1cd
Charlie Greenman <charie@razroo.com>
2018-12-10 21:42:21 -0500

    Remove the custom.d.ts file conflicting with build.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {ChooseSize} from "./common/ui/choose-size/choose-size";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <ChooseSize/>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
